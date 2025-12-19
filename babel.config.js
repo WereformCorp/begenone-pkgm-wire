4408d03d6dd3715cd62b623a07404438cc0e1f05
areeshalam <areeshpersonal5@gmail.com>
2025-12-19 16:25:46 +1100

    build: update dist

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "transform-rename-import",
+      {
+        original: "\\.jsx$",
+        replacement: ".js",
+      },
+    ],
+  ],
 };
